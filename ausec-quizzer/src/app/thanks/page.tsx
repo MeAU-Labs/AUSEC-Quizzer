0c90d800084019e7337fe56a37c46473d45c0eef
Aryan Iyappan <aryaniyappan2006@gmail.com>
2024-10-11 22:13:40 +0530

    add support email

--- ausec-quizzer/src/app/thanks/page.tsx
+++ ausec-quizzer/src/app/thanks/page.tsx
@@ -1,8 +1,15 @@
+import { FEEDBACK_EMAIL } from "~/utils/constants";
+
 export default async function QuizThanksPage() {
   return (
     <div className="flex h-full w-full flex-col items-center justify-center gap-2">
       <h1>Thanks for participating in this quiz!</h1>
-      <p>Send feedback to EMAIL_GOES_HERE</p>
+      <p>
+        Want to report issues? Send feedback{" "}
+        <a className="text-primary" href={`mailto:${FEEDBACK_EMAIL}`}>
+          here
+        </a>
+      </p>
     </div>
   );
 }
--- ausec-quizzer/src/utils/constants.ts
+++ ausec-quizzer/src/utils/constants.ts
@@ -1,1 +1,3 @@
 export const APP_NAME = "AUSEC Quizzer";
+
+export const FEEDBACK_EMAIL = "aryaniyappan2006@gmail.com";
